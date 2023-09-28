6f8b6edbb4ea179510dad0cbdc6f69a37a668af6
Anonymous <>
2023-09-27 21:00:00 -0700

    work

--- public/js/base.js
+++ public/js/base.js
@@ -4,7 +4,8 @@ const config = {
     "Test Tasks": ["public/videos/box_high_level_test.mp4", "public/videos/box_low_level_test.mp4", "public/videos/couch_moving_high_level_test.mp4", "public/videos/couch_moving_low_level_test.mp4", "public/videos/stack6_high_level.mp4", "public/videos/stack6_low_level.mp4", "public/videos/drawer_high_level_test.mp4", "public/videos/drawer_low_level_test.mp4"],
   }
 }
-
+let visualize_hard_init_states = false;
+let visualize_easy_init_states = false;
 $(document).ready(function () {
   // var f = window.onscroll;
   let sectionBars = $(".barWrapper");
@@ -33,35 +34,63 @@ $(document).ready(function () {
   );
   window.scroll();
 
-  const srcs = document.getElementsByClassName("abstractexecutable")[0].getElementsByTagName("source")
-  const vids = document.getElementsByClassName("abstractexecutable")[0].getElementsByTagName("video")
+  // const srcs = document.getElementsByClassName("abstractexecutable")[0].getElementsByTagName("source")
+  // const vids = document.getElementsByClassName("abstractexecutable")[0].getElementsByTagName("video")
   // document.querySelector("source").src = "http://example.com/new_url.mp4"
 
-  function setVideos(cond) {
-    const videos = config.videos[cond]
-    if (cond === "Train Tasks") {
+  // function setVideos(cond) {
+  //   const videos = config.videos[cond]
+  //   if (cond === "Train Tasks") {
 
-    }
-    for (let i = 0; i < vids.length; i+=1) {
-      const vidElem = vids[i];
-      const source = vidElem.getElementsByTagName("source")[0]
-      vidElem.pause();
-      source.setAttribute("src", videos[i]);
-      vidElem.load();
-    }
-  }
-  setVideos("Test Tasks")
+  //   }
+  //   for (let i = 0; i < vids.length; i+=1) {
+  //     const vidElem = vids[i];
+  //     const source = vidElem.getElementsByTagName("source")[0]
+  //     vidElem.pause();
+  //     source.setAttribute("src", videos[i]);
+  //     vidElem.load();
+  //   }
+  // }
+  // setVideos("Test Tasks")
 
-  $("#task-select").on('change', function (e) {
-    var optionSelected = $("option:selected", this);
-    var valueSelected = this.value;
-    console.log({valueSelected})
-    setVideos(valueSelected)
-    if (valueSelected === "Train Tasks") {
-      
+  $("#visualize-hard-init-states").on('click', function (e) {
+    visualize_hard_init_states = !visualize_hard_init_states;
+    const el = document.getElementById("visualize-hard-init-states");
+    if (!visualize_hard_init_states) {
+      el.textContent = "Visualize Initial States"
+      let videos = $("#hard-videos video");
+    for (let i = 0; i < videos.length; i++) {
+      const element = videos[i];
+      element.play();
+    };
+    } else {
+      el.textContent = "Show Policy Trajectories"
+      let videos = $("#hard-videos video");
+    for (let i = 0; i < videos.length; i++) {
+      const element = videos[i];
+      element.pause();
+      element.currentTime = 0;
+    };
     }
-    else if (valueSelected === "Test Tasks") {
-      
+  })
+  $("#visualize-easy-init-states").on('click', function (e) {
+    visualize_easy_init_states = !visualize_easy_init_states;
+    const el = document.getElementById("visualize-easy-init-states");
+    if (!visualize_easy_init_states) {
+      el.textContent = "Visualize Initial States"
+      let videos = $("#easy-videos video");
+    for (let i = 0; i < videos.length; i++) {
+      const element = videos[i];
+      element.play();
+    };
+    } else {
+      el.textContent = "Show Policy Trajectories"
+      let videos = $("#easy-videos video");
+    for (let i = 0; i < videos.length; i++) {
+      const element = videos[i];
+      element.pause();
+      element.currentTime = 0;
+    };
     }
   })
 });
